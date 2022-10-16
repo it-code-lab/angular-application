137bc5006697febf3d108ec864889e6ae6d8c60c
Numero Uno <numerounodeveloper@gmail.com>
2022-10-16 12:14:23 -0400

    Route Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { HomeComponent } from './home/home.component';
 import { PipedemoComponent } from './pipedemo/pipedemo.component';
+import { RtguardGuard } from './rtguard.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', canActivate: [RtguardGuard], component: HomeComponent },
   { path: 'pipedemo', component: PipedemoComponent },
   { path: 'pipedemo/:id', component: PipedemoComponent },
   { path: '**', component: ErrorpageComponent }
